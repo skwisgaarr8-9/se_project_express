ad60b58a2699dc9a89bf89bebdbd91f1ea3dfd28
Francis Flanagan <7flanaganf@gmail.com>
2023-04-20 17:20:59 +0900

    features: implement rate limiter middleware and helmet middleware for headers security

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
 const routes = require("./routes/index");
 const { handleError } = require("./utils/errors");
-
+const { limiter } = require("./utils/rateLimiter");
 const { PORT = 3001 } = process.env;
 const app = express();
 
@@ -11,6 +12,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(helmet());
+app.use(limiter);
 app.use((req, res, next) => {
   req.user = {
     _id: "643e57f149f21c552a30cdfb",
